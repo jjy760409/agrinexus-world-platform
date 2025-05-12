d601d824088e15535c9792c4e5084481c6ea08a0
jjy760409 <jys760409@naver.com>
2025-05-12 15:37:25 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,14 @@
-self.addEventListener('install', e => {
+self.addEventListener("install", function(e) {
   e.waitUntil(
-    caches.open('agrinexus-live').then(cache => {
-      return cache.addAll(['/', '/index.html', '/style.css', '/script.js']);
+    caches.open("agrinexus-cache").then(function(cache) {
+      return cache.addAll(["/"]);
     })
   );
 });
-self.addEventListener('fetch', e => {
+self.addEventListener("fetch", function(e) {
   e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
   );
 });
