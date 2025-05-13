04b1618c0d92ddd0ea9e69d9be0b0799af657aea
jjy760409 <jys760409@naver.com>
2025-05-13 11:47:24 +0900

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,5 @@
-// Firebase 설정 (사용 전 실제 키로 교체 필요)
-const firebaseConfig = {
+firebase.initializeApp({
   apiKey: "AIzaSyXXXXXXX",
   authDomain: "agrinexus-world.firebaseapp.com",
-  projectId: "agrinexus-world",
-  storageBucket: "agrinexus-world.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcde123456"
-};
+  projectId: "agrinexus-world"
+});
