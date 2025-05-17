a0eb350ea0cfb4a6332fd289f2750a0a7520c82f
jjy760409 <jys760409@naver.com>
2025-05-17 21:28:21 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,9 @@
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('agrinexus-cache').then(cache => {
-      return cache.addAll(['index.html']);
-    })
+    caches.open('agrinexus-v2').then(cache => cache.addAll(['index.html']))
   );
 });
-
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
