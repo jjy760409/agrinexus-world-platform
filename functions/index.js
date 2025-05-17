074ad40c70f56a57f3d69cf50c3336bda7e270c8
jjy760409 <jys760409@naver.com>
2025-05-18 00:57:45 +0900

    Add files via upload

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,14 @@
 
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
 
-exports.helloAgriNexus = functions.https.onRequest((req, res) => {
-  res.send("AgriNexus Functions Active");
+exports.saveContract = functions.https.onRequest((req, res) => {
+  const content = req.body.content;
+  const ref = admin.database().ref("contracts").push();
+  ref.set({
+    content: content,
+    timestamp: new Date().toISOString()
+  });
+  res.json({ status: "OK", id: ref.key });
 });
