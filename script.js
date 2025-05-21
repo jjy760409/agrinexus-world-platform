d0e7dde584d3e5ccdb0acd3505ebf4310cecb12d
jjy760409 <jys760409@naver.com>
2025-05-21 13:49:08 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-
-window.addEventListener('DOMContentLoaded', () => {
-  const hour = new Date().getHours();
-  const root = document.documentElement;
-
-  if (hour >= 19 || hour < 6) {
-    root.setAttribute('data-theme', 'night');
-    document.getElementById('greeting').textContent = "🌙 AgriNexus World - 야간 모드";
-  } else {
-    document.getElementById('greeting').textContent = "🌤 AgriNexus World - 주간 모드";
-  }
-});
+<!-- 공통 스크립트 -->
